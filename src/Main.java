479b766387f04c7f9cf14606d951512e25cd4f54
AdrianOlivaV <jao.vejar@gmail.com>
2024-07-10 22:26:57 -0700

    iniciando mi camino en java

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("bien venidos a screen match");
+        System.out.println("Pelicula: Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        boolean incuidoEnElPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0 + 7.8 )/ 4;
+
+        System.out.println("calificacion de la pelicula: "+media);
+        String sinopsis= """
+                Matrix es una paradoja 
+                la mejor pelicula del fin del milenio
+                fue lanzada en: 
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+        int clasificacion = (int) (media/2);
+        System.out.println(clasificacion);
+
+       /* ejercicio de practica
+       int celcius = 35;
+        double fahrenheit = (celcius*1.8)+32;
+
+        System.out.println("la temeperature de " + celcius + " grados celcius, equivale a " + fahrenheit+ " grados fahrenheit");
+        */
+    };
+}
