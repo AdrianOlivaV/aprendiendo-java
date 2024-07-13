8c5eeda5da57c8f6989b6061235a120edc5f0ef7
AdrianOlivaV <jao.vejar@gmail.com>
2024-07-13 11:00:09 -0700

    probando ciclos de repeticions

--- src/Loops.java
+++ src/Loops.java
@@ -5,5 +5,16 @@ public class Loops {
         Scanner teclado=new Scanner(System.in);
 
 
+        double nota = 0;
+        double mediaEvaluacion=0;
+
+        //probando FOR
+        for (int i = 0; i < 3; i++) {
+            System.out.println("que nota le darias a la pelicula matrix");
+            nota=teclado.nextDouble();
+            mediaEvaluacion=mediaEvaluacion+nota;
+        }
+
+        System.out.println("la nota media de matrix es "+mediaEvaluacion/3);
     }
 }
